4c9d2cf7c05adcad028846e0bf85b6a6a425786b
brett <brett.w.taylor@gmail.com>
2024-07-15 11:26:42 -0400

    add vagrant-vmware-utility to pipeline

--- scripts/build_box_vmware.sh
+++ scripts/build_box_vmware.sh
@@ -4,6 +4,7 @@ set -e
 packer init ./vagrant-vmware.pkr.hcl
 packer validate ./vagrant-vmware.pkr.hcl
 vagrant plugin install vagrant-vmware-desktop
+sudo systemctl restart vagrant-vmware-utility
 packer build \
     -color=false \
     -on-error=abort \
