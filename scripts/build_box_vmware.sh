6a8916c09dd58573d4965e889867da768af80e46
brett <brett.w.taylor@gmail.com>
2024-07-15 15:07:47 -0400

    try again

--- scripts/build_box_vmware.sh
+++ scripts/build_box_vmware.sh
@@ -3,7 +3,7 @@ set -e
 
 packer init ./vagrant-vmware.pkr.hcl
 packer validate ./vagrant-vmware.pkr.hcl
-vagrant plugin install vagrant-vmware-desktop
+vagrant plugin install --plugin-version=3.0.1 vagrant-vmware-desktop
 packer build \
     -color=false \
     -on-error=abort \
