e5eebf9e3560255d1179a038c770a8f3078e98c5
Brett Taylor <brett.w.taylor@gmail.com>
2024-07-16 10:18:17 -0400

    try a different box

--- scripts/build_box_vmware.sh
+++ scripts/build_box_vmware.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# set -e
+set -e
 
 packer init ./vagrant-vmware.pkr.hcl
 packer validate ./vagrant-vmware.pkr.hcl
 vagrant plugin install vagrant-vmware-desktop
 packer build \
     -color=false \
+    -on-error=abort \
     ./vagrant-vmware.pkr.hcl
-    # -on-error=abort \
-
-find . -name "*.log" -exec cat {} \;
